f14027a331b9e2f1c7519aceb39b6f165a426233
kyuuki <kyuuki.japan@gmail.com>
2023-06-08 05:21:10 +0000

    システムテスト追加

---
+++ test/system/static_pages_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class StaticPagesTest < ApplicationSystemTestCase
+  test "visiting root" do
+    visit root_url
+
+    assert_selector "h1", text: "ルートページ"
+  end
+end
