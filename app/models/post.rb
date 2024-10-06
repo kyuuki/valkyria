db6fbe544e03db90ccaa5715a1dcb69aea8d08a8
kyuuki <kyuuki.japan@gmail.com>
2024-10-06 14:42:48 +0000

    ブログの文字数制限を 1000 → 10000 に変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :sites
 
   validates :title, presence: true, length: { maximum: 80 }
-  validates :content, presence: true, length: { maximum: 1000 }
+  validates :content, presence: true, length: { maximum: 10000 }
   validates :posted_at, presence: true
 end
