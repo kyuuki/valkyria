ee405e95e5c93478e97f68cb75eecf0296ae8e23
kyuuki <kyuuki.japan@gmail.com>
2025-06-21 20:28:15 +0000

    管理画面のホスト名制限

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,13 @@
 class Admin::ApplicationController < ActionController::Base
   # ApplicationController は継承しない (完全に切り離す)
   layout "admin"
+
+  before_action :check_host
+
+  # 特定のホスト名でしかアクセスできないように
+  def check_host
+    unless (request.host == ENV["ADMIN_HOSTNAME"])
+      raise ActionController::BadRequest
+    end
+  end
 end
